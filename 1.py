85d99f13d6dc1951c148c9e9b3306f76807cbbbd
furik52 <vladfurov41@gmail.com>
2024-10-25 13:58:05 +0300

    calculator

--- 1.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-import os
-import sys
-import webbrowser
-import pyttsx3 as p
-from datetime import datetime
-import time
-import datetime
-import random
--- functions.py
+++ functions.py
@@ -28,3 +28,80 @@ speak_engine.setProperty('voice', voices[2].id)
 r = sr.Recognizer()
 m = sr.Microphone(device_index=1)
 voice = "str"
+def speak(what):
+     print(what)
+     speak = wincl.Dispatch("SAPI.SpVoice")
+     speak.Speak(what)
+
+def callback(recognizer, audio):
+    try:
+        global voice
+        voice = recognizer.recognize_google(audio, language="ru-RU").lower()
+
+        print("[log] Распознано: " + voice)
+
+        if voice.startswith(opts["alias"]):
+            cmd = voice
+
+            for x in opts['alias']:
+                cmd = cmd.replace(x, "").strip()
+
+            for x in opts['tbr']:
+                cmd = cmd.replace(x, "").strip()
+            voice = cmd
+            # распознаем и выполняем команду
+            cmd = recognize_cmd(cmd)
+            execute_cmd(cmd['cmd'])
+
+
+    except sr.UnknownValueError:
+        print("[log] Голос не распознан!")
+    except sr.RequestError as e:
+        print("[log] Неизвестная ошибка, проверьте интернет!")
+def listen():
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+    stop_listening = r.listen_in_background(m, callback)
+    while True: time.sleep(0.1)
+
+def recognize_cmd(cmd):
+    RC = {'cmd': '', 'percent': 0}
+    for c, v in opts['cmds'].items():
+        for x in v:
+            vrt = fuzz.ratio(cmd, x)
+            if vrt > RC['percent']:
+                RC['cmd'] = c
+                RC['percent'] = vrt
+    return RC
+def execute_cmd(cmd):
+    global startTime
+    if cmd == 'ctime':
+        now = datetime.datetime.now()
+        speak("Сейчас {0}:{1}".format(str(now.hour), str(now.minute)))
+    elif cmd == 'shutdown':
+        os.system('shutdown -s')
+        speak("Выключаю...")
+    elif cmd == 'calc':
+        calc.calculator()
+    elif cmd == 'conv':
+        convert.convertation()
+    elif cmd == 'translator':
+        translate.translate()
+    elif cmd == 'stupid1':
+        anekdot.fun()
+    elif cmd == 'internet':
+        browser.browser()
+    elif cmd == 'startStopwatch':
+        speak("Секундомер запущен")
+        startTime = time.time()
+    elif cmd == "stopStopwatch":
+        if startTime != 0:
+            Time = time.time() - startTime
+            speak(f"Прошло {round(Time // 3600)} часов {round(Time // 60)} минут {round(Time % 60, 2)} секунд")
+            startTime = 0
+        else:
+            speak("Секундомер не включен")
+    elif cmd == 'deals':
+        speak("Пока отлично.")
+    else:
+        print("Команда не распознана!")
